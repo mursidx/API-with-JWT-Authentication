760635c52d23816371d7066f5e71d42380b31f68
mursidx <iammursid@gmail.com>
2024-10-19 07:38:34 +0530

    changed PORT

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config();
-const debuglog = require('debug')('development:app');
-const cookieParser = require('cookie-parser');
-const authRoutes = require('./routes/authRoutes')
+require("dotenv").config();
+const debuglog = require("debug")("development:app");
+const cookieParser = require("cookie-parser");
+const authRoutes = require("./routes/authRoutes");
 
-const connectDb = require('./config/mongoose-connection');
+const connectDb = require("./config/mongoose-connection");
 connectDb();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
+app.use("/api/auth", authRoutes);
 
-app.use('/api/auth', authRoutes);
-
-app.listen(3000)
+app.listen(process.env.PORT || 3000);
